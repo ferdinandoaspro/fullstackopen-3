a334477167a1d4e020b1848ad348469ab37642a9
ferdinandoaspro <ferdinandoaspro2@gmail.com>
2024-06-24 16:16:41 +0200

    exercise 3.7 completed

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require("express")
+const morgan = require("morgan")
 const app = express()
 
 let persons = [
@@ -23,6 +24,9 @@ let persons = [
       "number": "39-23-6423122"
     }
 ]
+
+app.use(morgan("tiny"))
+
 
 app.get('/info', (req, res) => {
     res.end(`<p>Phonebook has info for ${persons.length} people</p><p>${new Date().toString()}</p>`)
